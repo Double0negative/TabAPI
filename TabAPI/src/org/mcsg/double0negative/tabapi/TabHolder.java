2a46bba62df60e675c3c33bbbb4434c095c6be76
Double0negative <soccerkid90@earthlink.net>
2013-03-02 01:18:26 -0500

    Initial Commit (B 0.1.15)

---
+++ TabAPI/src/org/mcsg/double0negative/tabapi/TabHolder.java
@@ -0,0 +1,17 @@
+package org.mcsg.double0negative.tabapi;
+
+import org.bukkit.plugin.Plugin;
+/**
+ * 
+ * 
+ * Holds tab information such as plugin and tab strings
+ * 
+ * 
+ * @author Drew
+ *
+ */
+class TabHolder{
+	Plugin p;
+	String[][]tab;
+	int maxh = 0, maxv = 0;
+}
---
+++ TabAPI/src/org/mcsg/double0negative/tabapi/TabObject.java
@@ -0,0 +1,71 @@
+package org.mcsg.double0negative.tabapi;
+
+import java.util.HashMap;
+
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+public class TabObject{
+
+
+	/**
+	 * Holds a list of tab information for a player.
+	 * 
+	 * 
+	 */
+	
+	
+	HashMap<Integer, TabHolder>tabs = new HashMap<Integer, TabHolder>();
+
+	
+	
+
+	public void setPriority(Plugin p, int pri){
+		//System.out.println("Settings pri for "+p.getName()+": "+pri);
+		for(int a = -1; a<4;a++){
+			if(tabs.get(a) != null && tabs.get(a).p == p){
+				tabs.put(a, null);
+			}
+		}
+		if(pri > -2){
+			TabHolder t = new TabHolder();
+			t.tab = new String[3][20];
+			t.p = p;
+			tabs.put(pri,t );
+		}
+	}
+
+	public TabHolder getTab(){
+		int a = 3;
+		while(tabs.get(a) == null && a >-3){
+			a--;
+		}
+		if(a == -2){
+			TabHolder  t = new TabHolder();
+			t.tab = new String[3][20];
+			return t;
+		}
+		//System.out.print("Getting tab "+a+" "+tabs.get(a).p.getName());
+		return tabs.get(a);
+	}
+
+	public void setTab(Plugin plugin, int x, int y, String msg) {
+		int a = -1;
+		while((tabs.get(a)==null || tabs.get(a).p != plugin) && a <3){
+			a++;
+		}
+		if(a == 3 && (tabs.get(a)==null||tabs.get(a).p != plugin)){
+			//System.out.println("Reseting Pri");
+			setPriority(plugin, 0);
+			a = 0;
+		}
+		//System.out.println(plugin.getName()+": "+a);
+
+		TabHolder t = tabs.get(a);
+
+		t.tab[y][x] = msg;
+		t.maxh = 3;
+		t.maxv = Math.max(x+1, t.maxv);
+	}
+
+}
